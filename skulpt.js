f1f6140a0b5021ff5fa74be7f2c2e17c278144ff
ankurankan <ankurankan@gmail.com>
2013-09-09 15:01:56 +0530

    some testing

--- skulpt.js
+++ skulpt.js
@@ -12017,7 +12017,7 @@ copy_to_trace = function(trace, temp_trace){
 	empty_values["event"] = temp_trace["trace"]["event"];
 	trace.trace.push(empty_values)
 	return(trace);
-} *///
+} **/
 //////////////////////////////////////variables////////////////////////////
 var unique_hash_no = 1;
 var frame_id = 1;
